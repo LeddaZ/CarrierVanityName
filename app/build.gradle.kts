4794c490f4f9aec5dc84e0e2a34f3b0d2af61c93
Leonardo Ledda <leonardoledda@gmail.com>
2025-07-17 14:04:13 +0200

    Migrate Gradle files to Kotlin and other stuff
    * Update to JDK 21
    * Set versionCode to commit count
    * Add commit hash to version name
    
    Signed-off-by: Leonardo Ledda <leonardoledda@gmail.com>

---
+++ app/build.gradle.kts
@@ -0,0 +1,80 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+fun execCommand(command: String): String? {
+    val cmd = command.split(" ").toTypedArray()
+    val process = ProcessBuilder(*cmd)
+        .redirectOutput(ProcessBuilder.Redirect.PIPE)
+        .start()
+    return process.inputStream.bufferedReader().readLine()?.trim()
+}
+
+val commitCount by project.extra {
+    execCommand("git rev-list --count HEAD")?.toInt()
+        ?: throw GradleException("Unable to get number of commits. Make sure git is initialized.")
+}
+
+val commitHash by project.extra {
+    execCommand("git rev-parse --short HEAD")
+        ?: throw GradleException(
+            "Unable to get commit hash. Make sure git is initialized."
+        )
+}
+
+android {
+    namespace = "gay.nullby.carriername"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "gay.nullby.carriername"
+        minSdk = 28
+        targetSdk = 36
+        versionCode = commitCount
+        versionName = "1.1 ($commitHash)"
+    }
+
+    buildTypes {
+        getByName("release") {
+            // Includes the default ProGuard rules files.
+            setProguardFiles(
+                listOf(
+                    getDefaultProguardFile("proguard-android-optimize.txt")
+                )
+            )
+        }
+        getByName("debug") {
+            applicationIdSuffix = ".dev"
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
+    }
+
+    kotlin {
+        compilerOptions {
+            jvmTarget = JvmTarget.fromTarget("21")
+        }
+    }
+
+    buildFeatures {
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation(libs.androidx.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    implementation(libs.androidx.constraintlayout)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
+    implementation(libs.shizuku.api)
+    implementation(libs.shizuku.provider)
+    implementation(libs.lsposed.hiddenapibypass)
+}
